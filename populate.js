ed268ba24aa0a47d692ed97eea4a3c34c2bd09fd
NocTuro974 <31305710+NocTuro974@users.noreply.github.com>
2018-03-23 14:46:35 +0100

    C:/Program Files/Git/populate et /search ajoutés et fonctionnels

--- populate.js
+++ populate.js
@@ -1,16 +1,42 @@
 const async = require('async');
 const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
+const elasticsearch = require('elasticsearch');
 
-async function asyncGetBrands(){
-  const brands = await getBrands();
-  console.log(brands);
-}
+var modelList = [];
 
 async function asyncGetModels(){
-  const models = await getModels('PEUGEOT');
-  console.log(models);
+  let models = await getModels('PEUGEOT');
+  models.forEach(element => {
+    let index = {index : {_index : "models", _type : "model", _id : element.uuid}};
+    modelList.push(index);
+    modelList.push(element);
+  });
+  console.log(modelList);
 }
 
-//start();
+async function main(){
+  
+  var client = new elasticsearch.Client({
+    host : 'localhost:9200',
+    log : 'trace'
+  });
+
+  await asyncGetModels();
+
+  console.log("Début de l'indexage")
+  client.bulk({
+    body : modelList
+  },
+  function(error, response){
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log(response);
+        console.log("indexation terminée");
+    }
+  })
+}
+
 asyncGetModels();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,71 @@
 var express = require('express');
 var router = express.Router();
+const elasticsearch = require('elasticsearch');
+const async = require('async');
+const {getBrands} = require('node-car-api');
+const {getModels} = require('node-car-api');
+
+var client = new elasticsearch.Client({
+    host : 'localhost:9200',
+    log : 'trace'
+})
+
+var modelList = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send("working");
 });
+
+router.get('/populate', async function(req, res, next) {
+    await populate();
+    res.send("indexage terminé");
+});
+
+router.get('/search', async function(req, res, next){
+    let qres = await search();
+    res.send("recherche terminée\n" + qres);
+});
+
+async function asyncGetModels(){
+    let models = await getModels('PEUGEOT');
+    models.forEach(element => {
+        let index = {index : {_index : "models", _type : "model", _id : element.uuid}};
+        modelList.push(index);
+        modelList.push(element);
+    });
+}
+
+async function populate(){
+    console.log("Récupération des modèles")
+    await asyncGetModels();
+
+    console.log("Début de l'indexage")
+    client.bulk({
+        body : modelList
+    },
+    function(error, response){
+        if (error){
+            console.log(error);
+        }
+        else{
+            console.log(response);
+            console.log("indexation terminée");
+        }
+    });
+}
+
+async function search(){
+    let res = await client.search({
+        index : "models",
+        type : "model",
+        q : "brand:PEUGEOT"
+    });
+    console.log("passe ici");
+    res.hits.hits.forEach(function(element){
+        console.log(element._source)
+    })
+    return res.hits;
+}
 
 module.exports = router;
